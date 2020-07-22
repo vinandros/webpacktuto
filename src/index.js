c7559a5e00a2059ffd599a3c839a273a0b1b9444
vinandros <vinandros@gmail.com>
2020-07-22 14:07:59 -0600

    Asset Management

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import _ from 'lodash'
+import _ from 'lodash';
+import "./style.css";
+import Icon from './icon.png';
+import Data from './data.xml';
+
 
 function component() {
     const element = document.createElement('div');
@@ -6,6 +10,14 @@ function component() {
     // Lodash, currently included via a script, is required for this line to work
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+    // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+    
+    console.log(Data);
   
     return element;
   }
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,42 @@ const { NamedModulesPlugin } = require("webpack");
 module.exports = {
     entry: "./src/index.js",
     output:{
-        filename: "main.js",
+        filename: "bundle.js",
         path: path.resolve(__dirname,"dist")
     },
-} 
+    module:{
+        rules:[
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+            {
+                test:/\.(png|svg|jpg|gif)$/,
+                use:[
+                    "file-loader"
+                ]
+            },
+            {
+                test:/\.(woff|woff2|eot|ttf|otf)$/,
+                use:[
+                    "file-loader"
+                ]
+            },
+            {
+            test: /\.(csv|tsv)$/,
+                use: [
+                'csv-loader',
+                ],
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                'xml-loader',
+                ],
+            },
+        ]
+    }
+};
