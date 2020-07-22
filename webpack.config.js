4183401c3c638893291890e2d81b36bdfbd9fada
vinandros <vinandros@gmail.com>
2020-07-22 15:27:57 -0600

    development

--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,26 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 
 module.exports = {
+    mode:"development",
     entry: {
         app: "./src/index.js",
         print: "./src/print.js"
     },
+    devtool:'inline-source-map',
+    devServer: {
+        contentBase:"./dist",
+    },
     plugins: [ 
-        new CleanWebpackPlugin(),
+        new CleanWebpackPlugin({ 
+            cleanStaleWebpackAssets: false 
+        }),
         new htmlWebpackPlugin({
             title:"Output Management"
         }),
     ],
     output:{
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname,"dist")
+        path: path.resolve(__dirname,"dist"),
+        publicPath: "/",
     }
 };
